58a131f8b08e69eca1f3765415288d8d25e61cd2
huang <569747259@qq.com>
2018-05-07 17:40:23 +0800

    完善分页数据显示

--- my-project/src/components/module/pageCount.vue
+++ my-project/src/components/module/pageCount.vue
@@ -1,33 +1,47 @@
 <template>
     <div class="page-count">
-        <span>01</span>
+        <span>{{ this.page }}</span>
         <span class="line"></span>
         <span>04</span>
     </div>
 </template>
 
 
+<script>
+    export default {
+        data() {
+            return {
+            };
+        },
+
+        props: ['page']
+    };
+
+</script>
+
+
 <style lang="scss" scoped>
-.page-count {
-    z-index: 99;
-    width: 100px;
-    height: 70px;
-    background: #1f1f1f;
-    position: absolute;
-    top: 50%;
-    right: 0;
-    transform: translateY(-50%);
-    font-weight: normal;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    .line {
-        width: 50px;
-        height: 2px;
-        background: #fff;
-        margin: 5px 0;
-        border-radius: 15px;
+    .page-count {
+        z-index: 99;
+        width: 100px;
+        height: 70px;
+        background: #1f1f1f;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        transform: translateY(-50%);
+        font-weight: normal;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .line {
+            width: 50px;
+            height: 2px;
+            background: #fff;
+            margin: 5px 0;
+            border-radius: 15px;
+        }
     }
-}
+
 </style>
