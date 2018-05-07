3cfcbc74d28a77fe20383b6d17b3813bb91f8f6a
huang <569747259@qq.com>
2018-05-07 21:22:03 +0800

    关于我页面：中间第一个版块完成

--- my-project/src/components/aboutMe/center.vue
+++ my-project/src/components/aboutMe/center.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="center">
-        <div class="info">
-            <img src="../../assets/img/photo.png" alt="">
-            <div class="rename">
-                <span>R</span>
-                <span>Resume</span>
+        <div :class="[resumeClass, other]" @click="openResume">
+            <!-- <img v-show="resumeClass" src="../../assets/img/photo.png" alt=""> -->
+            <div class="title">
+                <span class="name">R</span>
+                <span v-show="resumeClass">Resume</span>
             </div>
 
-            <div class="info-show">
-                <div class="name">
+            <div class="resume-show" v-show="resumeClass">
+                <div class="user-name">
                     <span>Name</span>
                     <div>
                         <span>张文圣</span>
@@ -20,7 +20,7 @@
                     </div>
                 </div>
 
-                <div class="age">
+                <div class="user-age">
                     <span>Age</span>
                     <div>
                         <span>24</span>
@@ -28,26 +28,75 @@
                     </div>
                 </div>
 
-                <div class="education">
-                    <span>College Graduate</span>
+                <div class="user-education">
+                    <span>Education</span>
                     <div>
-                        <span>24</span>
-                        <span class="remark">(1993/12/10)</span>
+                        <span>College Graduate</span>
+                        <span class="remark">(Sichuan Univasity Science & Engineering)</span>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="other1 other">C</div>
-        <div class="other2 other">A</div>
-        <div class="other3 other">S</div>
+        <div :class="[companyClass, other]" class="bg2" @click="openCompany">
+            <div class="title">
+                <span class="name">C</span>
+                <span v-show="companyClass">Company</span>
+            </div>
+        </div>
+        <div :class="[abilityClass, other]" class="bg3" @click="openAbility">
+            <div class="title">
+                <span class="name">A</span>
+                <span v-show="abilityClass">Ability</span>
+            </div>
+        </div>
+        <div :class="[skillClass, other]" class="bg4" @click="openSkill">
+            <div class="title">
+                <span class="name">S</span>
+                <span v-show="skillClass">Skill</span>
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script>
     export default {
-
+        data() {
+            return {
+                other: "other",
+                resumeClass: "resumeClass", // 点击R显示个人信息(默认)
+                companyClass: "", // 点击C显示个人信息
+                abilityClass: "", // 点击A显示信息
+                skillClass: "", // 点击S显示技能信息
+            };
+        },
+        methods: {
+            openResume() {
+                this.resumeClass = "resumeClass";
+                this.companyClass = "";
+                this.abilityClass = "";
+                this.skillClass = "";
+            },
+            openCompany() {
+                this.companyClass = "companyClass";
+                this.resumeClass = "";
+                this.abilityClass = "";
+                this.skillClass = "";
+            },
+            openSkill() {
+                this.skillClass = "skillClass";
+                this.resumeClass = "";
+                this.abilityClass = "";
+                this.companyClass = "";
+            },
+            openAbility() {
+                this.abilityClass = "abilityClass";
+                this.resumeClass = "";
+                this.skillClass = "";
+                this.companyClass = "";
+            }
+        }
     };
 
 </script>
@@ -63,18 +112,31 @@
         display: flex;
     }
 
-    .remark{
+    .other {
+        padding: 2rem;
+        width: 15%;
+        height: 100%;
+        color: #474747;
+        font-weight: 700;
+        text-align: center;
+
+    }
+
+    .remark {
         @include fontToEmpty(1rem);
         color: #fff;
         opacity: .5;
+        // white-space: nowrap;
+        margin-right: 20px;
     }
 
-    .rename {
+    .title {
         color: #fff;
         font-size: 2rem;
         position: relative;
-        span:first-child {
-            @include fontToEmpty(5rem);
+        text-align: center;
+        .name {
+            @include fontToEmpty(7rem);
             color: #fff;
             opacity: 0.2;
         }
@@ -85,15 +147,21 @@
         }
     }
 
-    .info {
-        padding: 3rem;
+    %selCommon {
+        padding: 2rem;
         flex: 1;
         display: flex;
         flex-direction: column;
-        justify-content: space-around;
-        background: #101010;
+        justify-content: space-between;
         overflow: hidden;
         position: relative;
+        transition: all 300ms linear;
+        .title {
+            text-align: left;
+        }
+    } // 个人信息
+    .resumeClass {
+        @extend %selCommon;
         img {
             position: absolute;
             top: -10rem;
@@ -102,9 +170,10 @@
         }
     }
 
-    .info-show {
+    .resume-show {
         display: flex;
         justify-content: space-between;
+        text-align: left;
         width: 60%;
         span {
             display: block;
@@ -119,30 +188,30 @@
             >div {
                 margin-bottom: 1.5rem;
             }
-            span:first-child{
+            span:first-child {
                 font-weight: 600;
             }
         }
+    } // 就职公司
+    .companyClass {
+        @extend %selCommon;
+    } // 个人能力
+    .abilityClass {
+        @extend %selCommon;
+    } // 个人技能
+    .skillClass {
+        @extend %selCommon;
     }
 
-    .other {
-        width: 15%;
-        height: 100%;
-        color: #474747;
-        font-weight: 700;
-        font-size: 7rem;
-        text-align: center;
-    }
-
-    .other1 {
+    .bg2 {
         background: #191919;
     }
 
-    .other2 {
+    .bg3 {
         background: #111;
     }
 
-    .other3 {
+    .bg4 {
         background: #191919;
     }
 
