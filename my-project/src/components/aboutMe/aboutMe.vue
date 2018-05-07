9c5c595e8f817092985594d6b65ccf46ec34ccaa
huang <569747259@qq.com>
2018-05-07 17:42:06 +0800

    完成关于我页面1.0

--- my-project/src/components/aboutMe/aboutMe.vue
+++ my-project/src/components/aboutMe/aboutMe.vue
@@ -1,20 +1,48 @@
 <template>
     <div id="aboutMe">
-
+        <Header name="about me"></Header>
+        <Center></Center>
+        <page-count page="03"></page-count>
+        <Footer></Footer>
     </div>
 </template>
 
 
 <script>
-let data = {};
-export default {
-    data() {
-        return data;
-    }
-};
+    import Header from "../module/header";
+    import Center from "./center";
+    import Footer from "../module/footer";
+    import pageCount from "../module/pageCount";
+
+
+    let data = {};
+    export default {
+        data() {
+            return data;
+        },
+        components: {
+            Header,
+            Center,
+            Footer,
+            pageCount
+        }
+
+    };
+
 </script>
 
 
 <style lang="scss" scoped>
+    #aboutMe {
+        height: 100vh;
+        width: 100vw;
+        background-color: #2c2c2c;
+        position: relative;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        overflow: hidden;
+    }
 
 </style>
